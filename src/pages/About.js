52ab1f30b56cb7f4d2024ef0cf159b6d42d4e125
Dmitri So <dmitriso@yahoo.com>
2021-02-09 17:45:20 -0500

    Troubleshooting and minor styling adjustments.

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,10 @@ import React from "react";
 
 const About = () => (
     <div className="lead background-screen text-color">
+        <div className="mt">
         <img src="https://ca.slack-edge.com/T01E837E8MP-U01DX9F95JB-6fd5aa71b157-512" className="float-left pr-3 img-fluid" alt="Photo Of Dmitri"></img>
         <p>Migrating from Maryland to Philadelphia about three years ago. Before enrolling in UPenn's LPS Coding Bootcamp, I was a licensed Cosmetologist (Hairstylist) for about seven years. Due to the global pandemic, I was presented an opportunity to make a career change. Always having a passion for coding, tech, and gaming. I have accumulated a trove of useful knowledge and technical skills. Teaching my self-everything from basic Web-Development to Leatherworking. I have also owned and ran my own business for several years as well. Incorporating my creative affinity to design and handcrafting, I have always sought to conceive and construct from the ground up. Being a natural problem solver, passionate learner, and a resourceful individual is second nature to me. With hard work, dedication, and passion I expect these qualities to show case my commitment to the industry. I hope my diverse experiences have provided me with a unique thought process to take me far as a Web Developer. I love coding, designing, and all forms of creation. Surrounded by many experienced peers and mentors, I am extremely excited to become a UPenn LPS Full Stack Web Developer so I can work on the forefronts of this Digital Age! Please enjoy portfolio and feel free to contact me for any opportunities and/or messages.</p>
+        </div>
     </div>
 );
 
