9d318d5274cae8278d12aac81e0ebc20f40e141e
Dmitri So <dmitriso@yahoo.com>
2021-07-06 20:20:20 -0400

    Importing project images as modules and passing as props, Error Handling.

--- src/utils/DKS.js
+++ src/utils/DKS.js
@@ -1,36 +1,44 @@
+import img1 from "../assets/images/In-Solez.jpg"
+import img2 from "../assets/images/THme1.jpg"
+import img3 from "../assets/images/Progressive-Budget-Tracker.jpg"
+import img4 from "../assets/images/Good-Burger.jpg"
+import img5 from "../assets/images/Fitness-Tracker.jpg"
+import img6 from "../assets/images/Schedule-day-planner1.jpg"
+
+
  const DKS = [
     {
         "id": 1,
         "name": "In-Solez",
-        "image": "./assets/images/In-Solez.jpg",
+        "image": img1,
         "repo": "https://github.com/dmitriso/In-Solez",
         "deployed": "https://in-solez.herokuapp.com/"
     },
     {
         "id": 2,
         "name": "THme",
-        "image": "../assets/images/THme1.jpg",
+        "image":img2,
         "repo": "https://github.com/dmitriso/THme",
         "deployed": "https://dmitriso.github.io/THme/"
     },
     {
         "id": 3,
         "name": "Progressive-Budget-Tracker",
-        "image": "./assets/images/Progressive-Budget-Tracker.jpg",
+        "image":img3,
         "repo": "https://github.com/dmitriso/Progressive-Budget-Tracker",
         "deployed":"https://dry-everglades-61211.herokuapp.com/"
     },
     {
         "id": 4,
         "name": "Good-Burger",
-        "image": "./assets/images/Good-Burger.jpg",
+        "image":img4,
         "repo": "https://github.com/dmitriso/Good-Burger",
         "deployed":"https://aqueous-headland-38171.herokuapp.com/"
     },
     {
         "id": 5,
         "name": "Fitness-Tracker",
-        "image": "./assets/images/Fitness-Tracker.jpg",
+        "image":img5,
         "repo": "https://github.com/dmitriso/Fitness-Tracker",
         "deployed":"https://salty-hamlet-96659.herokuapp.com/"
 
@@ -38,7 +46,7 @@
     {
         "id": 6,
         "name": "Schedule-Of-TheDay",
-        "image":"./assets/images/Schedule-day-planner1.jpg",
+        "image": img6,
         "repo": "https://github.com/dmitriso/Schedule-Of-TheDay",
         "deployed":"https://dmitriso.github.io/Schedule-Of-TheDay/"
     }
