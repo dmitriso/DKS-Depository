4c3f3b928d0c2bd0e828a1fd6f2561d6069f75ca
Dmitri So <dmitriso@yahoo.com>
2021-07-07 15:47:15 -0400

    Fixing error when clicking the resume link, pdf file is now imported as a component.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,13 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import "./styles.css";
+import resume from "../../assets/resume/WebDevelopmentResume(2021).pdf"
 
 const About = () => (
     <div className="lead background-screen text-color">
         <div className="mt">
             <h1 className="mono-font">About me</h1>
-            <p>Phone:(443)-558-6397 | Email: Dmitriso77@gmail.com | <a href="./assets/resume/WebDevelopmentResume(2021).pdf" className="resume" target="_blank">Resume</a></p>
+            <p>Email: Dmitriso77@gmail.com | <a href={resume} rel="noreferrer" target="_blank">Resume</a></p>
         <p>Migrating from Maryland to Philadelphia about three years ago. Before enrolling in UPenn's LPS Coding Bootcamp, I was a licensed Cosmetologist (Hairstylist) for about seven years. Always having a passion for coding, tech, and gaming. I have accumulated a trove of useful knowledge and technical skills. Teaching myself everything from basic Web-Development to Leatherworking. I have also owned and ran my own business for several years as well. Incorporating my creative affinity to design and handcrafting, I have always sought to conceive and construct from the ground up. Being a natural problem solver, passionate learner, and a resourceful individual is second nature to me. With hard work, dedication, and passion I expect these qualities to show case my commitment to the industry. I hope my diverse experiences have provided me with a unique thought process to take me far as a Web Developer. I love coding, designing, and all forms of creation. Surrounded by many experienced peers and mentors, I am extremely excited to become a UPenn LPS Full Stack Web Developer so I can work on the forefronts of this Digital Age! Please enjoy portfolio and feel free to contact me for any opportunities and/or messages.</p>
         </div>
     </div>
