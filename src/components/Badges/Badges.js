fceb79f52aa24b547a2a93ad646aadeb0b13b991
Dmitri So <dmitriso@yahoo.com>
2021-07-07 18:31:41 -0400

    Created component to display tech badges.

---
+++ src/components/Badges/Badges.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import "./Badges.css";
+
+function Badges() {
+    return (
+        <div>
+            <h3></h3>
+            <div className="badge-table">
+                <div>
+                    <h5>Programming Languages</h5>
+                    <section className="badge-list">
+                        <div className="badge-item">
+                            <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="HTML" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt="CSS" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="Javascript" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="	https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" alt="Node" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                    </section>
+                </div>
+                <div>
+                    <h5>Frameworks</h5>
+                    <section className="badge-list">
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                    </section></div>
+                <div>
+                    <h5>Version Control</h5>
+                    <section className="badge-list">
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                    </section>
+                </div>
+                <div>
+                    <h5>Databases</h5>
+                    <section className="badge-list">
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="badge-item">
+                            <img src="" alt="" />
+                        </div>
+                    </section>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Badges;
+
+
+
