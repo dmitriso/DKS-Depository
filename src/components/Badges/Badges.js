931a035fc7cc920a4c3ede5d8fc2d64283197042
Dmitri So <dmitriso@yahoo.com>
2021-07-08 12:50:16 -0400

    Adding more badges for tech skills.

--- src/components/Badges/Badges.js
+++ src/components/Badges/Badges.js
@@ -7,7 +7,7 @@ function Badges() {
             <h3></h3>
             <div className="badge-table">
                 <div>
-                    <h5>Programming Languages</h5>
+                    <h5>Languages</h5>
                     <section className="badge-list">
                         <div className="badge-item">
                             <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="HTML" />
@@ -30,29 +30,29 @@ function Badges() {
                     <h5>Frameworks</h5>
                     <section className="badge-list">
                         <div className="badge-item">
-                            <img src="" alt="" />
+                            <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="React" />
                         </div>
                         <div className="badge-item">
-                            <img src="" alt="" />
+                            <img src="https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" alt="Express" />
                         </div>
                         <div className="badge-item">
-                            <img src="" alt="" />
+                            <img src="https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white" alt="JQuery" />
                         </div>
                         <div className="badge-item">
-                            <img src="" alt="" />
+                            <img src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" alt="Bootstrap" />
                         </div>
                         <div className="badge-item">
                             <img src="" alt="" />
                         </div>
                     </section></div>
                 <div>
-                    <h5>Version Control</h5>
+                    <h5>Tech</h5>
                     <section className="badge-list">
                         <div className="badge-item">
-                            <img src="" alt="" />
+                            <img src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" alt="Heroku" />
                         </div>
                         <div className="badge-item">
-                            <img src="" alt="" />
+                            <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub" />
                         </div>
                         <div className="badge-item">
                             <img src="" alt="" />
@@ -69,10 +69,10 @@ function Badges() {
                     <h5>Databases</h5>
                     <section className="badge-list">
                         <div className="badge-item">
-                            <img src="" alt="" />
+                            <img src="https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white" alt="MongoDB" />
                         </div>
                         <div className="badge-item">
-                            <img src="" alt="" />
+                            <img src="https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white" alt="MySQL" />
                         </div>
                         <div className="badge-item">
                             <img src="" alt="" />
