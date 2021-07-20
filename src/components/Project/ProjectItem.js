a46497e0ec4c4243ba8a7f02755cc531657dc0e7
Dmitri So <dmitriso@yahoo.com>
2021-07-20 15:49:30 -0400

    Adding description to project cards structure and props/Changing component styling.

--- src/components/Project/ProjectItem.js
+++ src/components/Project/ProjectItem.js
@@ -11,10 +11,10 @@ function ProjectItem(props) {
           </div>
           <div className='card-back text-center'>
             <h1>{props.name}</h1>
-            <a target='__blank' href={props.deployed}>Deployed App</a>
+            <p>{props.description}</p>
+            <a target='__blank' href={props.deployed}><i className="fa fa-window-maximize" aria-hidden="true"></i> Deployed</a>
             <br></br>
-            <a target='__blank' href={props.repo}>GitHub Repository</a>
-            <br></br>
+            <a target='__blank' href={props.repo}><i className="fa fa-github" aria-hidden="true"></i> SourceCode</a>
           </div>
         </div>
       </div>
