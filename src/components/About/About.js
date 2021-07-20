36ecf94cc19da13858e60bddd1b28896a839bb6a
Dmitri So <dmitriso@yahoo.com>
2021-07-20 15:45:42 -0400

    Adding a description to component structure and props/Changing card styling .

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,8 +9,8 @@ const About = () => (
         
         <div className="mt">
             <h1 className="mono-font">About me</h1>
-            <p>Email: Dmitriso77@gmail.com | <a href={resume} rel="noreferrer" target="_blank">Resume</a></p>
-        <p>A Fullstack Web Developer focused on UX/UI, passionate about coding, tech, gaming, and fashion. I've accumulated a trove of theoretical and technical skills. Teaching myself everything from Web-Development to Business Management. I have also owned and ran my own business for several years as well. Incorporating my creative affinity to design and handcrafting, I have always sought to conceive and construct from the ground up. Being a natural problem solver, passionate learner, and a resourceful individual is second nature to me. With hard work, dedication, and passion I expect these qualities to show case my commitment to the industry. I hope my diverse experiences have provided me with a unique thought process to take me far as a Web Developer. I love coding, designing, and all forms of creation. Surrounded by many experienced peers and mentors, I am extremely excited to become a UPenn LPS Full Stack Web Developer so I can work on the forefronts of this Digital Age! Please enjoy portfolio and feel free to contact me for any opportunities and/or messages.</p>
+            <p>Dmitriso77@gmail.com | <a href={resume} rel="noreferrer" target="_blank">Resume</a></p>
+        <p>A Fullstack Web Developer focused on reshaping the digital human experience. Incorporating my creative affinity to design and entrepreneurial background, I have always sought to conceive and construct from the ground up. I love coding, designing, gaming, aquascaping, and fashion. I am extremely excited to be a Web Developer apart of your team!, or helping you bring your digital ideas to reality! </p>
         </div>
     </div>
 );
