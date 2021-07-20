1ff43e3d5fc9726f868bb6c04f8e2b2e4df88b64
Dmitri So <dmitriso@yahoo.com>
2021-07-20 15:56:11 -0400

    Adding descriptions to projects.

--- src/utils/DKS.js
+++ src/utils/DKS.js
@@ -13,35 +13,40 @@ import img7 from "../assets/images/RydrMainPage.jpg"
         "name": "In-Solez",
         "image": img1,
         "repo": "https://github.com/dmitriso/In-Solez",
-        "deployed": "https://in-solez.herokuapp.com/"
+        "deployed": "https://in-solez.herokuapp.com/",
+        "description" : "A social networking platform that lets users view a database of sneakers, save it to their collection, showcase, compare, trade, and sell sneakers with other users."
     },
     {
         "id": 2,
         "name": "THme",
         "image":img2,
         "repo": "https://github.com/dmitriso/THme",
-        "deployed": "https://dmitriso.github.io/THme/"
+        "deployed": "https://dmitriso.github.io/THme/",
+        "description": "This app was built to recommend medical cannabis strains for the user's ailments. Based on the user symptoms, certain medicinal cannabis strains and herbal remedies are recommended."
     },
     {
         "id": 3,
         "name": "Rydr",
         "image": img7,
         "repo": "https://github.com/dmitriso/Rydr",
-        "deployed":"https://still-reaches-69009.herokuapp.com/#/"
+        "deployed":"https://still-reaches-69009.herokuapp.com/#/",
+        "description": ""
     },
     {
         "id": 4,
         "name": "Good-Burger",
         "image":img4,
         "repo": "https://github.com/dmitriso/Good-Burger",
-        "deployed":"https://aqueous-headland-38171.herokuapp.com/"
+        "deployed":"https://aqueous-headland-38171.herokuapp.com/",
+        "description": ""
     },
     {
         "id": 5,
         "name": "Fitness-Tracker",
         "image":img5,
         "repo": "https://github.com/dmitriso/Fitness-Tracker",
-        "deployed":"https://salty-hamlet-96659.herokuapp.com/"
+        "deployed":"https://salty-hamlet-96659.herokuapp.com/",
+        "description": ""
 
     },
     {
@@ -49,14 +54,16 @@ import img7 from "../assets/images/RydrMainPage.jpg"
         "name": "Schedule-Of-TheDay",
         "image": img6,
         "repo": "https://github.com/dmitriso/Schedule-Of-TheDay",
-        "deployed":"https://dmitriso.github.io/Schedule-Of-TheDay/"
+        "deployed":"https://dmitriso.github.io/Schedule-Of-TheDay/",
+        "description": ""
     },
     {
         "id": 7,
         "name": "Progressive-Budget-Tracker",
         "image":img3,
         "repo": "https://github.com/dmitriso/Progressive-Budget-Tracker",
-        "deployed":"https://dry-everglades-61211.herokuapp.com/"
+        "deployed":"https://dry-everglades-61211.herokuapp.com/",
+        "description": ""
     }
 
 ]
