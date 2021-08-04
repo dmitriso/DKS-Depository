a290625f7b18e8422b255f8b1b53975ef27b83ed
Dmitri So <dmitriso@yahoo.com>
2021-08-04 15:05:57 -0400

    Format clean up / Adding new elements

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,13 +1,22 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from "react";
-import "./Welcome.css";
+import React from 'react';
+import './Welcome.css';
 
 const Welcome = () => (
-    <div className="background-screen welcome-el">
-        {/* <h1 className="hero">CODING, CREATION, AND CRAFT</h1>
-        <a href="#" className="hero-btn"><span>About Me</span></a> */}
-        <img src="https://ca.slack-edge.com/T01E837E8MP-U01DX9F95JB-6fd5aa71b157-512" className="float-left pr-3 img-fluid round-img middle" alt="Photo Of Dmitri"></img>
-    </div>
-)
+  <div className='background-screen welcome-el'>
+    {/* <section>
+      <h1 className='hero'>CODING, CREATION, AND CRAFT</h1>
+      <a href='#' className='hero-btn'>
+        <span>About Me</span>
+      </a>
+    </section> */}
 
-export default Welcome
+    <img
+      src='https://ca.slack-edge.com/T01E837E8MP-U01DX9F95JB-6fd5aa71b157-512'
+      className='float-left pr-3 img-fluid round-img middle'
+      alt='Photo Of Dmitri'
+    ></img>
+  </div>
+);
+
+export default Welcome;
