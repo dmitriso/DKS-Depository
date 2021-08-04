8bfc6aea1f0ffc1a9db88358cc95dee33d8d8f56
Dmitri So <dmitriso@yahoo.com>
2021-08-04 15:03:20 -0400

    Error handling: Adding rel='noreffer' to a tags

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 import "./Footer.css";
 
@@ -7,10 +8,10 @@ function Footer() {
       <footer className="footer py-3 text-center mt-0 mono-font">
     <section className="container ">
       <span className="text-muted ">©Copyright 2020 Dmitri So</span>
-      <a href="https://stackoverflow.com/users/14544394/dmitriso" target="_blank"><i
+      <a href="https://stackoverflow.com/users/14544394/dmitriso" target="_blank" rel="noreferrer"><i
           className="fa fa-stack-overflow p-2 text-warning" aria-hidden="true"></i></a>
-      <a href="https://github.com/dmitriso" target="_blank"><i className="fa fa-github p-2 text-warning" aria-hidden="true"></i></a>
-      <a href="https://www.linkedin.com/in/dmitri-so-545b2167/" target="_blank"><i className="fa fa-linkedin p-2 text-warning"
+      <a href="https://github.com/dmitriso" target="_blank" rel="noreferrer"><i className="fa fa-github p-2 text-warning" aria-hidden="true"></i></a>
+      <a href="https://www.linkedin.com/in/dmitri-so-545b2167/" target="_blank" rel="noreferrer"><i className="fa fa-linkedin p-2 text-warning"
           aria-hidden="true"></i></a>
     </section>
   </footer>
